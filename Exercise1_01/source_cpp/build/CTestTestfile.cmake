d155b4d41a8180ad5f21fa46c9a36649a627b627
vuola <markus.vuolahti@gmail.com>
2022-06-30 17:28:44 +0200

    Ex1.02

--- Exercise1_01/source_cpp/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/vuola/UH/Kubernetes/UH_DevOpsKubernetes/Exercise1_01/source_cpp
-# Build directory: /Users/vuola/UH/Kubernetes/UH_DevOpsKubernetes/Exercise1_01/source_cpp/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
