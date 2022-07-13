96eda71ca2e0be91159e430e81899d90c37a2434
vuola <markus.vuolahti@gmail.com>
2022-07-13 08:23:22 +0200

    E1_11

---
+++ Exercise1_11/pingpong/html/index.php
@@ -0,0 +1,20 @@
+<?php 
+$filename = "/tmp/kube/counter.txt";
+
+if (file_exists($filename)) {
+    $fd = fopen ($filename, "r"); // opening the file in read mode
+    $contents = fread ($fd, filesize($filename)); // reading the content of the file
+    fclose ($fd); // Closing the file pointer
+} 
+else {
+    $contents = "0";
+}
+
+echo "PONG " . $contents;
+
+$contents = $contents + 1;
+$fp = fopen ($filename, "w"); // Open the file in write mode
+fwrite ($fp, $contents); // Write the new data to the file
+fclose ($fp); // Closing the file pointer
+
+?>
